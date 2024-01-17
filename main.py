e69941b06301999adc6bf8025841fbcf40982b5e
Gabriel Areco <areco23@gmail.com>
2024-01-17 17:00:04 -0400

    FEAT: set resolution and codec

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import cv2
-vid = cv2.VideoCapture(2)
-while(True):  
+my_cam_index = 2
+vid = cv2.VideoCapture(my_cam_index, cv2.CAP_V4L)
+vid.set(cv2.CAP_PROP_FPS, 10.0)
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 2048)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1536)
+vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
+while (True):
     ret, frame = vid.read()
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):

d0fa0cba1e20f064e68ac11aef5c4e0024aa2eef
Gabriel Areco <areco23@gmail.com>
2024-01-17 19:52:53 -0400

    CHORE: put controls on a separed file

---
+++ controls.py
@@ -0,0 +1,61 @@
+import cv2
+def controls(vid, key):
+        #Toggle autofocus
+        if key == ord('f'):
+            if vid.get(cv2.CAP_PROP_AUTOFOCUS) == 0.0:
+                vid.set(cv2.CAP_PROP_AUTOFOCUS, 1.0)
+                print("Autofocus on")
+            elif vid.get(cv2.CAP_PROP_AUTOFOCUS) == 1.0:
+                vid.set(cv2.CAP_PROP_AUTOFOCUS, 0.0)
+                print("Autofocus off")
+
+        #Control Focus
+        if key == ord('['):
+            focus = vid.get(cv2.CAP_PROP_FOCUS)
+            print("FOCUS: " + str(focus))
+            vid.set(cv2.CAP_PROP_FOCUS, focus-5)
+        if key == ord(']'):
+            focus = vid.get(cv2.CAP_PROP_FOCUS)
+            print("FOCUS: " + str(focus))
+            vid.set(cv2.CAP_PROP_FOCUS, focus+5)
+        
+        #Control exposure
+        if key == ord('u'):
+            exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
+            print("EXPOSURE: " + str(exposure))
+            vid.set(cv2.CAP_PROP_EXPOSURE, exposure-100)
+        if key == ord('i'):
+            exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
+            print("EXPOSURE: " + str(exposure))
+            vid.set(cv2.CAP_PROP_EXPOSURE, exposure+100)
+
+        #Control brightness
+        if key == ord('o'):
+            brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
+            print("BRIGHTNESS: " + str(brightness))
+            vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness-5)
+        if key == ord('p'):
+            brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
+            print("BRIGHTNESS: " + str(brightness))
+            vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness+5)
+
+        #Control gain 
+        if key == ord('j'):
+            gain = vid.get(cv2.CAP_PROP_GAIN)
+            print("GAIN: " + str(gain))
+            vid.set(cv2.CAP_PROP_GAIN, gain-5)
+        if key == ord('k'):
+            gain = vid.get(cv2.CAP_PROP_GAIN)
+            print("GAIN: " + str(gain))
+            vid.set(cv2.CAP_PROP_GAIN, gain+5)
+
+        #Control contrast 
+        if key == ord('l'):
+            contrast = vid.get(cv2.CAP_PROP_CONTRAST)
+            print("CONTRAST: " + str(contrast))
+            vid.set(cv2.CAP_PROP_CONTRAST, contrast-5)
+        if key == ord(';'):
+            contrast = vid.get(cv2.CAP_PROP_CONTRAST)
+            print("CONTRAST: " + str(contrast))
+            vid.set(cv2.CAP_PROP_CONTRAST, contrast+5)
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+
+from controls import controls
 cam_index = 2
 focus = 400.0
 contrast = 64.0
@@ -25,64 +27,7 @@ while (True):
     keys = cv2.waitKey(1) & 0xFF
     if keys == ord('q'):
         break
-
-    #Toggle autofocus
-    if keys == ord('f'):
-        if vid.get(cv2.CAP_PROP_AUTOFOCUS) == 0.0:
-            vid.set(cv2.CAP_PROP_AUTOFOCUS, 1.0)
-            print("Autofocus on")
-        elif vid.get(cv2.CAP_PROP_AUTOFOCUS) == 1.0:
-            vid.set(cv2.CAP_PROP_AUTOFOCUS, 0.0)
-            print("Autofocus off")
-
-    #Control Focus
-    if keys == ord('['):
-        focus = vid.get(cv2.CAP_PROP_FOCUS)
-        print("FOCUS: " + str(focus))
-        vid.set(cv2.CAP_PROP_FOCUS, focus-5)
-    if keys == ord(']'):
-        focus = vid.get(cv2.CAP_PROP_FOCUS)
-        print("FOCUS: " + str(focus))
-        vid.set(cv2.CAP_PROP_FOCUS, focus+5)
-    
-    #Control exposure
-    if keys == ord('u'):
-        exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
-        print("EXPOSURE: " + str(exposure))
-        vid.set(cv2.CAP_PROP_EXPOSURE, exposure-100)
-    if keys == ord('i'):
-        exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
-        print("EXPOSURE: " + str(exposure))
-        vid.set(cv2.CAP_PROP_EXPOSURE, exposure+100)
-
-    #Control brightness
-    if keys == ord('o'):
-        brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
-        print("BRIGHTNESS: " + str(brightness))
-        vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness-5)
-    if keys == ord('p'):
-        brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
-        print("BRIGHTNESS: " + str(brightness))
-        vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness+5)
-
-    #Control gain 
-    if keys == ord('j'):
-        gain = vid.get(cv2.CAP_PROP_GAIN)
-        print("GAIN: " + str(gain))
-        vid.set(cv2.CAP_PROP_GAIN, gain-5)
-    if keys == ord('k'):
-        gain = vid.get(cv2.CAP_PROP_GAIN)
-        print("GAIN: " + str(gain))
-        vid.set(cv2.CAP_PROP_GAIN, gain+5)
-
-    #Control contrast 
-    if keys == ord('l'):
-        contrast = vid.get(cv2.CAP_PROP_CONTRAST)
-        print("CONTRAST: " + str(contrast))
-        vid.set(cv2.CAP_PROP_CONTRAST, contrast-5)
-    if keys == ord(';'):
-        contrast = vid.get(cv2.CAP_PROP_CONTRAST)
-        print("CONTRAST: " + str(contrast))
-        vid.set(cv2.CAP_PROP_CONTRAST, contrast+5)
+    else:
+        controls(vid,keys)
 vid.release()
 cv2.destroyAllWindows()

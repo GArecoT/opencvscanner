7f7dab60284dbf72633b87403248db31f249dfda
Gabriel Areco <areco23@gmail.com>
2024-01-17 21:26:38 -0400

    FEAT: contours working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 from controls import controls
 cam_index = 2
@@ -23,10 +24,42 @@ vid.set(cv2.CAP_PROP_SATURATION, saturation)
 
 while (True):
     ret, frame = vid.read()
-    cv2.imshow('frame', frame)
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+    ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(thresh,kernel,iterations=2)
+    thresh = cv2.erode(imgDial,kernel,iterations=1)
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+    def get_contour_areas(contours):
+
+        all_areas= []
+
+        for cnt in contours:
+            area= cv2.contourArea(cnt)
+            all_areas.append(area)
+
+        return all_areas
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
+
+
+    largest_item= sorted_contours[0]
+
+    image_copy = frame.copy()
+    cv2.drawContours(image=image_copy, contours=largest_item, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+    x,y,w,h = cv2.boundingRect(largest_item)
+    ROI = frame[y:y+h, x:x+w]
+
+    # see the results
+    cv2.namedWindow('custom window', cv2.WINDOW_KEEPRATIO)
+    cv2.imshow('custom window', image_copy)
+    #cv2.imshow('None approximation', image_copy)
     keys = cv2.waitKey(1) & 0xFF
     if keys == ord('q'):
         break
+    elif keys == ord("d"):
+        cv2.imwrite('ROI.png',ROI)
     else:
         controls(vid,keys)
 vid.release()

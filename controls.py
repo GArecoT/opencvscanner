b355e88b94717f54b12c6f5785f8725a8dc931cc
Gabriel Areco Taveira <estagio.startup06@sesims.com.br>
2024-01-25 09:25:03 -0400

    feat: moved controls and cofigs to ini file

--- controls.py
+++ controls.py
@@ -3,8 +3,8 @@ from PIL import Image
 import os
 from tkinter import simpledialog
 from tkinter import messagebox
-def controls(vid, key, cut, count):
-        if key == ord('e'):
+def controls(vid, key, cut, count, config):
+        if key == ord(config.get('controls','toggleautoexposure')):
             if vid.get(cv2.CAP_PROP_AUTO_EXPOSURE) == 3.0:
                 vid.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1.0)
                 print("Auto Exposure Off")
@@ -13,7 +13,7 @@ def controls(vid, key, cut, count):
                 print("Auto Exposure On")
 
         #Toggle autofocus
-        if key == ord('f'):
+        if key == ord(config.get('controls','toggleautofocus')):
             if vid.get(cv2.CAP_PROP_AUTOFOCUS) == 0.0:
                 vid.set(cv2.CAP_PROP_AUTOFOCUS, 1.0)
                 print("Autofocus on")
@@ -22,60 +22,62 @@ def controls(vid, key, cut, count):
                 print("Autofocus off")
 
         #Control Focus
-        if key == ord('['):
+        if key == ord(config.get('controls','focus-')):
             focus = vid.get(cv2.CAP_PROP_FOCUS)
             print("FOCUS: " + str(focus))
             vid.set(cv2.CAP_PROP_FOCUS, focus-5)
-        if key == ord(']'):
+        if key == ord(config.get('controls','focus+')):
             focus = vid.get(cv2.CAP_PROP_FOCUS)
             print("FOCUS: " + str(focus))
             vid.set(cv2.CAP_PROP_FOCUS, focus+5)
         
         #Control exposure
-        if key == ord('u'):
+        if key == ord(config.get('controls','exposure-')):
             exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
             print("EXPOSURE: " + str(exposure))
             vid.set(cv2.CAP_PROP_EXPOSURE, exposure-100)
-        if key == ord('i'):
+        if key == ord(config.get('controls','exposure+')):
             exposure = vid.get(cv2.CAP_PROP_EXPOSURE)
             print("EXPOSURE: " + str(exposure))
             vid.set(cv2.CAP_PROP_EXPOSURE, exposure+100)
 
         #Control brightness
-        if key == ord('o'):
+        if key == ord(config.get('controls','brightness-')):
             brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
             print("BRIGHTNESS: " + str(brightness))
             vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness-5)
-        if key == ord('p'):
+        if key == ord(config.get('controls','brightness+')):
             brightness = vid.get(cv2.CAP_PROP_BRIGHTNESS)
             print("BRIGHTNESS: " + str(brightness))
             vid.set(cv2.CAP_PROP_BRIGHTNESS, brightness+5)
 
         #Control gain 
-        if key == ord('j'):
+        if key == ord(config.get('controls','gain-')):
             gain = vid.get(cv2.CAP_PROP_GAIN)
             print("GAIN: " + str(gain))
             vid.set(cv2.CAP_PROP_GAIN, gain-5)
-        if key == ord('k'):
+        if key == ord(config.get('controls','gain+')):
             gain = vid.get(cv2.CAP_PROP_GAIN)
             print("GAIN: " + str(gain))
             vid.set(cv2.CAP_PROP_GAIN, gain+5)
 
         #Control contrast 
-        if key == ord('l'):
+        if key == ord(config.get('controls','contrast-')):
             contrast = vid.get(cv2.CAP_PROP_CONTRAST)
             print("CONTRAST: " + str(contrast))
             vid.set(cv2.CAP_PROP_CONTRAST, contrast-5)
-        if key == ord(';'):
+        if key == ord(config.get('controls','contrast+')):
             contrast = vid.get(cv2.CAP_PROP_CONTRAST)
             print("CONTRAST: " + str(contrast))
             vid.set(cv2.CAP_PROP_CONTRAST, contrast+5)
-
-        if key == 92:
+        
+        #add page
+        if key == int(config.get('controls','addpage')): #this is the key code
             cv2.imwrite("./temp/" + str(str(count) + ".png"),cut)
             count = count + 1
             print("Page " + str(count) + " saved")
-        if key == 103:
+        #save file
+        if key == int(config.get('controls','savefile')): #this is the keycode
             images = []
             for i in range(count):
                 temp = Image.open("./temp/"+str(i)+".png")
@@ -101,9 +103,9 @@ def controls(vid, key, cut, count):
                     if os.path.isfile(os.path.join('./temp/', filename)):
                      os.remove(os.path.join('./temp/', filename))
             
-        if key == ord(','):
+        if key == ord(config.get('controls','rotate-90')):
             return -90, count
-        if key == ord('.'):
+        if key == ord(config.get('controls','rotate+90')):
             return 90, count
 
         return 0, count

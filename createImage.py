9b6673bd6ff48caeaeb1f4c3e5492f5c3a450e41
Gabriel Areco <areco23@gmail.com>
2026-02-04 11:41:08 -0400

    FEAT: Move pages saved with arrow keys!

--- createImage.py
+++ createImage.py
@@ -2,13 +2,47 @@ from threading import Thread
 from time import sleep, time
 from tkinter import Toplevel, Label
 from PIL import Image, ImageTk
+import os
 
 photo = None
 frame = None
 photoTK = None
 window = None
+root = None
+notification = None
+localImg = 0
 
 prevTime = time()
+
+def viwerControls(key):
+    global photo, frame, photoTK, root, localImg, notification
+    # go to next pag
+    if(key.keysym == 'Right'):
+        root.title(str(localImg))
+        localImg = int(localImg) + 1
+        # check if image exists
+        path = "./.temp/"+  str(localImg) +".png"
+        if(os.path.isfile(path)):
+            photo = Image.open(path)
+            photoTK = ImageTk.PhotoImage(image=photo, master=root)
+            frame.configure(image=photoTK)
+            notification.config(text="Page" + str(localImg))
+        else:
+            localImg = int(localImg) - 1
+
+    # go to prev pag
+    if(key.keysym == 'Left'):
+        root.title(str(localImg))
+        localImg = int(localImg) - 1
+        path = "./.temp/"+  str(localImg) +".png"
+        if(os.path.isfile(path)):
+            photo = Image.open(path)
+            photoTK = ImageTk.PhotoImage(image=photo, master=root)
+            frame.configure(image=photoTK)
+            notification.config(text="Page" + str(localImg))
+        else:
+            localImg = int(localImg) + 1
+
 
 def debounce(a):
     global prevTime, window
@@ -35,19 +69,28 @@ def resize_image():
     frame.image = photoTK
 
 def spawnImageViewer(img):
-    global photo, frame, photoTK
+    global photo, frame, photoTK, root, localImg, notification
+    localImg = int(img)
     root = Toplevel()
-    root.title(img)
+    root.title(str(localImg))
     root.attributes('-type', 'dialog')
     root.geometry("500x700")
     frame = Label(root)
-    photo = Image.open("./.temp/"+  str(img) +".png")
+    notification = Label(root, fg="#f5c2e7", bg="#313244")
+    notification.place(x=10, y=10)
+    photo = Image.open("./.temp/"+  str(localImg) +".png")
     # photo = Image.open(img)
     photoTK = ImageTk.PhotoImage(image=photo, master=root)
     frame.configure(image=photoTK)
     frame.bind('<Configure>', lambda a: Thread(target=debounce, args=(a,)).start())
     frame.pack(fill='both', expand=True)
+    root.bind(
+        "<Key>",
+        lambda event: 
+            viwerControls(event)
+    )
     root.mainloop()
+
 
 # spawnImageViewer('0')
 
